5ae67b10053617d77830d4698ccb97b123dacd3d
drrnmcdnld <drrnmcdnld@gmail.com>
2015-03-25 15:53:49 -0300

    Started to add in replacement methods and updated some of the index.html

---
+++ js/resumeBuilder.js
@@ -0,0 +1,4 @@
+var formattedName = HTMLheaderName.replace("%data%", "D.Darren MacDonald");
+var formattedRole = HTMLheaderRole.replace("%data%", "Front End Developer");
+$('#main').prepend(formattedRole);
+$('#main').prepend(formattedName);
