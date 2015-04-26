8e4167698efddcc2e13a3557a94251d37bb23f82
drrnmcdnld <drrnmcdnld@gmail.com>
2015-04-26 16:12:50 -0300

    Added some of the online courses that I took this year.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,7 +7,7 @@ String.prototype.replaceData = function(data)
 // JSON Data for Resume
 var bio = {
 	"name" : "Darren MacDonald",
-	"role" : "Web Developer",
+	"role" : "Web Developer - Experience Designer",
 	"contact" : {
 		"mobile" : "<a href='tel:9025636959'>902.563.6959</a>",
 		"email" : "<a href='mailto:drrnmcdnld@gmail.com'>drrnmcdnld@gmail.com</a>",
@@ -26,6 +26,7 @@ var bio = {
 	$("#header").prepend(HTMLheaderRole.replaceData(bio.role));
 	$("#header").prepend(HTMLheaderName.replaceData(bio.name));
 	$("#header").prepend(HTMLbioPic.replaceData(bio.pic));
+	
 	// Contact Info
 	$("#topContacts").prepend(HTMLmobile.replaceData(bio.contact.mobile));
 	$("#topContacts").prepend(HTMLemail.replaceData(bio.contact.email));
@@ -47,7 +48,7 @@ var work = {
 		"title" : "Managing Director",
 		"location" : "Sydney, Nova Scotia",
 		"dates" : "Apr 2011 - present",
-		"description" : "Launching a startup project can be overwhelming to say the least and starting as just a one-person shop can be even harder. I have been lucky to have the support of many friends, family and colleagues to rely on. To achieve this the focus will be on human-centred design and systems thinking. The products and services may change but the over-arching goal will remain the same. At present most of the services try to make 'work life' better but this may evolve over time working with our customers.  Being human-centred and holistic in our thinking will more than likely expand the services."	},
+		"description" : "Launching a startup project can be overwhelming to say the least and starting as just a one-person shop can be even harder. I have been lucky to have the support of many friends, family and colleagues to rely on. To achieve this the focus will be on human-centred design and systems thinking. The products and services may change but the over-arching goal will remain the same. At present most of the services try to make 'work life' better but this may evolve over time working with our customers. Being human-centred and holistic in our thinking will more than likely expand the services."	},
 	{
 		"employer" : "WCB of Nova Scotia",
 		"title" : "Relationship Manager, Ergonomist",
@@ -111,44 +112,76 @@ var education = {
 		"location" : "Sydney, Nova Scotia",
 		"dates" : "Sep 2014 - present",
 		"degree" : "BBA",
-		"major" : "Information Technology",
+		"major" : ["Information Technology"],
 		"url" : "http://UITstartup.org"	},
 		{
 		"name" : "University of Derby",
 		"location" : "Derby, United Kingdom",
 		"dates" : "Sep 2006 - Sep 2010",
 		"degree" : "MSc",
-		"major" : "Ergonomics",
+		"major" : ["Ergonomics"],
 		"url" : "http://derby.ac.uk/"	},
 		{
 		"name" : "Dalhousie University",
 		"location" : "Halifax, Nova Scotia",
-		"dates" : "Sep 1991 - May 2006",
+		"dates" : "Sep 1991 - May 1996",
 		"degree" : "BSc",
-		"major" : "Kinesiology",
+		"major" : ["Kinesiology"],
 		"url" : "http://dal.ca"	}
 	],
 
 	"onlineCourses" : [
+
 	{
-		"title" : "Javascript Basics",
-		"school" : "Udacity",
-		"dates" : "Feb 2014",
-		"url" : "http://UITstartup.org"	
+		"title" : "HTML & CSS",
+		"school" : "Codecademy",
+		"dates" : "Aug 2014",
+		"url" : "http://www.codecademy.com/tracks/web"	
 	},
-	{
-		"title" : "Javascript Basics",
-		"school" : "Udacity",
-		"dates" : "Feb 2014",
-		"url" : "http://UITstartup.org"	
+
+{
+		"title" : "HTML/CSS Path - Master Status",
+		"school" : "Code School",
+		"dates" : "Feb 2015",
+		"url" : "https://www.codeschool.com/paths/html-css"	
 	},
-	{
-		"title" : "Javascript Basics",
+
+{
+		"title" : "Intro to HTML/CSS",
 		"school" : "Udacity",
-		"dates" : "Feb 2014",
-		"url" : "http://UITstartup.org"	
+		"dates" : "Feb 2015",
+		"url" : "https://www.udacity.com/course/intro-to-html-and-css--ud304"	
+	},
+
+{
+		"title" : "Responsive Web Design Fundamentals",
+		"school" : "Udacity",
+		"dates" : "Feb 2015",
+		"url" : "https://www.udacity.com/course/responsive-web-design-fundamentals--ud893"	
+	},
+
+{
+		"title" : "Javascript Basis",
+		"school" : "Udacity",
+		"dates" : "Mar 2015",
+		"url" : "https://www.udacity.com/course/javascript-basics--ud804"	
+	},
+
+{
+		"title" : "Intro to jQuery",
+		"school" : "Udacity",
+		"dates" : "Mar 2015",
+		"url" : "https://www.udacity.com/course/intro-to-jquery--ud245"	
+	},
+
+{
+		"title" : "Project Ninja - AngularJS & Firebase",
+		"school" : "Code4Startup",
+		"dates" : "Apr 2015",
+		"url" : "https://code4startup.com/projects/ninja-learn-angularjs-firebase-by-cloning-udemy"	
 	}
 	],
+	
 	"display" : function(){
 	// Education
 	for(school in education.schools) {
@@ -180,7 +213,6 @@ var map = {
 };
 
 // This block called Display Functions
-
 
 education.display();
 //map.display();
